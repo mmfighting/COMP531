18fbe3b995ac736e41424f0dc30ccc25e6f3356e
lulu <hanluzju@gmail.com>
2016-09-15 17:50:43 -0500

    delete unnecessary files

--- inclass-8/particle-inclass/tests.webpack.js
+++
@@ -1,4 +0,0 @@
-var context = require.context('./src', true, /^((?!index.js).)*\.jsx?$/);
-//var context = require.context('./src', true, /ttt.spec.js/);
-//var context = require.context('./src', true, /.*\.jsx?$/);
-context.keys().forEach(context);

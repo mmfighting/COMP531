ff170eac6556c25199a1c420fcfaaecce4dc7f18
mmfighting <lh11@Lus-MacBook.local>
2016-11-09 02:39:53 -0600

    add upload image function

--- hw6/frontend/src/components/article/newArticle.js
+++ hw6/frontend/src/components/article/newArticle.js
@@ -24,8 +24,10 @@ class NewArticle extends Component {
             <div className="well">
                 <div>
                     <h5>Post something:</h5>
-                    <textarea class="newPostBody" id="newPostBody"
-                              cols="80" rows="4" placeholder="Anything interesting"
+                    <textarea class="newPostBody"
+                              id="newPostBody"
+                              rows="4"
+                              placeholder="Anything interesting"
                               value={ this.message }
                               onChange={(e) => {
                                   this.message = e.target.value
@@ -34,7 +36,7 @@ class NewArticle extends Component {
                     </textarea>
                 </div>
                 <div>
-                    Add a image<input type="file" id="articleImage" accept="image/*" onChange={(e) => this.handleImageChange(e)}/>
+                    <input type="file" id="articleImage" accept="image/*" onChange={(e) => this.handleImageChange(e)}/>
                 </div>
                 { !this.file && !this.message ? '' :
                     <div>
@@ -49,17 +51,16 @@ class NewArticle extends Component {
                         </div>
                     </div>
                 }
-            </div>
-
-            { !this.file ? '' :
-                <div className="row">
-                    <img className="postImage" src={this.preview}/>
-                    <div>
-                        { this.file.webkitRelativePath || this.file.name }<br/>
-                        ({ parseInt(this.file.size / 1024 * 100)/100.0 } kB)
+                { !this.file ? '' :
+                    <div className="row">
+                        <img className="postImage" src={this.preview}/>
+                        <div>
+                            { this.file.webkitRelativePath || this.file.name }
+                            ({ parseInt(this.file.size / 1024 * 100)/100.0 } kB)
+                        </div>
                     </div>
-                </div>
-            }
+                }
+            </div>
         </div>
     )}
 }
